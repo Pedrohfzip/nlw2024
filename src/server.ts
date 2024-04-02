a5e99a63f3727f69b57a0c210860ebf3c029ecb3
Pedrohfzip <pedrohfagundes2002@gmail.com>
2024-04-01 22:22:20 -0300

    postConcluida

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 const app = fastify();
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+app.post("/events", async (req, res) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
 
-app.get("/", () => {
-  return "Hello nlw";
+  const data = createEventSchema.parse(req.body);
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  });
+
+  return res.status(201).send({ ebentId: event.id });
 });
 
 app

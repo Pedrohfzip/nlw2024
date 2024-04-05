1fe93ab36ccb9ccc8cfcfaf7a7835819647137c9
Pedrohfzip <pedrohfagundes2002@gmail.com>
2024-04-05 17:49:34 -0300

    cadastroParticipanteNoEvento

--- src/server.ts
+++ src/server.ts
@@ -4,63 +4,16 @@ import {
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
-import { generateString } from "./utils/generate-slug";
+import { createEventeRoute } from "./routes/create-event";
+import { registerForEvent } from "./routes/resgister-for-event";
+
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
-
-//Erro 200 sucesso
-//erro 300 Redirecionamento
-//Erro 400 erro informação enviada por cliente que chamou a api
-//erro 500 erro servidor, independente do que esta sendo enviado para os servidor
-
-app.withTypeProvider<ZodTypeProvider>().post(
-  "/events",
-  {
-    schema: {
-      body: z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-      }),
-      response: {
-        201: z.object({
-          eventId: z.string().uuid(),
-        }),
-      },
-    },
-  },
-  async (req, res) => {
-    const { title, details, maximumAttendees } = req.body;
-
-    const slug = generateString(title);
-    const eventWithSameSlug = await prisma.event.findUnique({
-      where: {
-        slug,
-      },
-    });
-    if (eventWithSameSlug != null) {
-      throw new Error("Another event with same title already exists");
-    }
-    const event = await prisma.event.create({
-      data: {
-        title: title,
-        details: details,
-        maximumAttendees: maximumAttendees,
-        slug,
-      },
-    });
-
-    return res.status(201).send({ ebentId: event.id, title: event.title });
-  }
-);
+app.register(createEventeRoute);
+app.register(registerForEvent);
 
 app
   .listen({ port: 3333 })
